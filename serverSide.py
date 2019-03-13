bf2ca127a3b389b30783325312a55c2a29b7afc2
Ali Babayev <ali.babayev.ug@gmail.com>
2019-03-13 18:37:46 +0300

    Updated Server side

--- serverSide.py
+++ serverSide.py
@@ -7,7 +7,7 @@ from subprocess import call
 import os
 
 host = ''
-port = 8000
+port = 7000
 # Start a socket listening for connections on 0.0.0.0:8000 (0.0.0.0 means
 # all interfaces)
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,27 +41,34 @@ try:
         # Rewind the stream, open it as an image with PIL and do some
         # processing on it
         image_stream.seek(0)
-
+        
         imageName = "t/" + str(count) + 'out.jpeg'
-
+        
         image = Image.open(image_stream).save(imageName)
-
+        
         pathname = "./darknet detector test cfg/READY.data cfg/READY.cfg backup/READY_20000.weights " + imageName + " -thresh 0.05"
-
+        
         call([pathname],shell = True)
         try:
             with open('text1.txt') as f:
                 mylist = f.read().splitlines()
+                if mylist.size >= 4 :
+                    reply = 'l ' + mylist[0] + ' r ' + mylist[1] + ' t ' + mylist[2]+ ' b ' + mylist[3]
+                    print('Image is verified')
+                    connection1.send(str.encode(reply))
+                    print('Data has been sent!\n')
+                else :
+                    print('Image is verified')
+                    connection1.send(str.encode("NO INPUT"))
+                    print('Data has been sent!\n')
         except:
             print('File could not open. No such a file.')
-        reply = 'l ' + mylist[0] + ' r ' + mylist[1] + ' t ' + mylist[2]+ ' b ' + mylist[3]
+            print('Image is verified')
+            connection1.send(str.encode("NO INPUT"))
+            print('Data has been sent!\n')
 
         #print('Image is %dx%d' % image.size)
         #image.verify()
-        print('Image is verified')
-        connection1.send(str.encode(reply))
-        print('Data has been sent!\n')
-
         count = count + 1
 finally:
     connection.close()
