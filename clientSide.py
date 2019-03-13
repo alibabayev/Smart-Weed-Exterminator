10e078951be0d1425c104cbded641147b7a54682
Ali Babayev <ali.babayev.ug@gmail.com>
2019-03-13 18:38:26 +0300

    Added servo motor code

--- clientSide.py
+++ clientSide.py
@@ -3,11 +3,19 @@ import socket
 import struct
 import time
 import picamera
+import RPi.GPIO as GPIO
 
 # Connect a client socket to my_server:8000 (change my_server to the
 # hostname of your server)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('169.254.145.159', 8000))
+client_socket.connect(('169.254.236.148', 7000))
+
+servoPIN = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+p.start(2.5) # Initialization
 
 # Make a file-like object out of the connection
 connection = client_socket.makefile('wb')
@@ -39,17 +47,28 @@ try:
         connection.write(stream.read())
         reply = client_socket.recv(1024)
         print(reply.decode('utf-8'))
+        
+        #servo motor code starts
+        p.ChangeDutyCycle(7.5)  # turn towards 90 degree
+        time.sleep(1) # sleep 1 second
+        p.ChangeDutyCycle(2.5)  # turn towards 0 degree
+        time.sleep(1) # sleep 1 second
+        p.ChangeDutyCycle(12.5) # turn towards 180 degree
+        time.sleep(1) # sleep 1 second 
+        #servo motor code end
+        
         # Reset the stream for the next capture
         stream.seek(0)
         stream.truncate()
-
+        
         endTime = time.time() - startTime
         print(endTime)
         count = count + 1
         startTime = time.time()
     # Write a length of zero to the stream to signal we're done
     connection.write(struct.pack('<L', 0))
-
 finally:
     connection.close()
     client_socket.close()
+    p.stop()
+    GPIO.cleanup()
