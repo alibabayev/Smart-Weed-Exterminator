653ced676d59f3182a3804dafb0bccb062e2860e
Ali Babayev <ali.babayev.ug@gmail.com>
2019-10-04 17:26:55 +0300

    Update serverSide.py

--- serverSide.py
+++ serverSide.py
@@ -6,52 +6,80 @@ from PIL import Image
 from subprocess import call
 import os
 
-WIDTH = 51 #width of land that the picture takes in cm  -> TBD DURING TEST
-HEIGHT = 60 #length of the land that the picture takes in cm  ->  TBD DURING TEST
-SPEED = 20 #speed of robot in cm/sec -> TBD DURING TEST
-COL = 3
+WIDTH = 50 #width of land that the picture takes in cm  -> TBD DURING TEST
+HEIGHT = 54 #length of the land that the picture takes in cm  ->  TBD DURING TEST
+SPEED = 11 #speed of robot in cm/sec -> TBD DURING TEST
+COL = 2
 ROW = 3
+ROADlength = HEIGHT * 5
 
-CELLwidth = 11
-pixelW = 720
-pixelH = 540
-cellHeight = 20
-TOTALROW = int(240 // cellHeight)
-startLimitPixelX = int((WIDTH - (COL * CELLwidth))/2)
-endLimitPixelX = int(WIDTH - (WIDTH - (COL * CELLwidth))/2)
-picture = [[0 for i in range(COL)] for j in range(TOTALROW)]
+CELLwidth = int(40 / COL)
+cellHeight = int(HEIGHT / ROW) #18
+pixelW = 2880
+pixelH = 2160
+TOTALROW = int(ROADlength // cellHeight)
+startLimitPixelX = int((WIDTH - (COL * CELLwidth))/2) #10
+endLimitPixelX = int(WIDTH - (WIDTH - (COL * CELLwidth))/2) #40
+matrix = [[0 for i in range(COL)] for j in range(TOTALROW)]
+print(*matrix)
 t2= time.time()
-delay = 0 #TBD in test case till server connects
-t0 = 3
-length = 196 # length of the are
+#delay = 0 #TBD in test case till server connects
+#t0 = 3
+#length = 196 # length of the are
 
 def colorPicture(counter2, label, x1, x2, y1, y2):#function to call everytime you take a picture and get positions of weed or cabbage
+    print("CheckList1")
+    print(str(counter2))
+    print(str(label))
+    print(str(x1))
+    print(str(x2))
+    print(str(y1))
+    print(str(y2))
     #t1 = time.time()
     #currentPosition = SPEED*(t1-t0)
     #currentRow = int(currentPosition / CELLwidth)
+    
+    print("CheckList2")
     realX1 = int((WIDTH * x1) / pixelW)
     realX2 = int((WIDTH * x2) / pixelW)
     realY1 = int((HEIGHT * y1) / pixelH)
     realY2 = int((HEIGHT * y2) / pixelH)
     
-    if realX2 >= startLimitPixelX && realX1 =< endLimitPixelX :
-        if realX1 < startLimitPixelX :
-            realX1 = startLimitPixelX
+    print("CheckList3")
+    if realX2 >= startLimitPixelX and realX1 <= endLimitPixelX :
         
-        if realX2 > endLimitPixelX :
-            realX2 = endLimitPixelX
+        print("CheckList4")
+        if realX1 <= startLimitPixelX :
+            print("CheckList5")
+            realX1 = startLimitPixelX + 0.01
         
-        X1 = int( realX1/ CELLwidth)
-        X2 = int( realX2 / CELLwidth)
-        Y1 = int(( realY1 // cellHeight ) + (counter-1) * 3)
-        Y2 = int(( realY2 // cellHeight ) + (counter-1) * 3)
+        print("CheckList6")
+        if realX2 >= endLimitPixelX :
+            print("CheckList7")
+            realX2 = endLimitPixelX - 0.01
+        print("CheckList8")
         
-        for i in range(X1,X2+1):
-            for j in range(Y1, Y2+1):
-                if (label == "cabbage"):
-                    picture[i][j] = -1
-                elif (label == "mint" && picture[i][j] != -1):
-                    picture[i][j] = -2
+        print("realX1 " + str(realX1) + "; realX2 " + str(realX2) + "; realY1 " + str(realY1) + "; realY2 " + str(realY2))
+        
+        X1 = int( (realX1 - startLimitPixelX) / CELLwidth)
+        X2 = int( (realX2 - startLimitPixelX) / CELLwidth)
+        Y1 = int(( realY1 / cellHeight )) #+  (counter2 - 1) * ROW
+        Y2 = int(( realY2 / cellHeight )) #+ (counter2 - 1) * ROW )
+        
+
+        print("X1 " + str(X1) + "; X2 " + str(X2) + "; Y1 " + str(Y1) + "; Y2 " + str(Y2))
+        
+        print("CheckList9")
+        for i in range(Y1,Y2+1):
+            print("CheckList10")
+            for j in range(X1, X2+1):
+                print("CheckList11")
+                if (label == -2):
+                    print("CheckList12")
+                    matrix[(counter2 - 1) * ROW + (ROW - 1 - i)][j] = -2
+                elif label == -1 and matrix[(counter2 - 1) * ROW + (ROW - 1 - i)][j] != -2:
+                    print("CheckList13")
+                    matrix[(counter2 - 1) * ROW + (ROW - 1 - i)][j] = -1
         print("colorPicture Function ended")
 
 """
@@ -61,24 +89,49 @@ def colorPicture(counter2, label, x1, x2, y1, y2):#function to call everytime yo
     based on the speed. TBD
     """
 def spray(counter1):
+    #print("Spray CheckList1")
     tempStr = ""
+    #print("Spray CheckList2")
     #DELAY = 0 # delay till the sprayers reach starting position.
     #t1 = time.time() + DELAY
     #currentPosition = int(SPEED*(t1-t0))
     #currentRow = int(currentPosition // 7)
-    retList = []
-    for i in range (0, ROW):
-        for j in range (0, COL):
-            if (picture[(counter-1) * 3 + i)][j] != -2 :
-                tempStr = tempStr + "0"
-            else :
-                tempStr = tempStr + "2"
-            tempStr = tempStr + " "
-                #retList.append(i)
+    #retList = []
+    
+    if counter1 == 1 :
+        #print("Spray CheckList3")
+        tempStr = tempStr + "0 0 0 0 0 0 "
+        #print("Spray CheckList4")
+        for i in range (1,ROW):
+            #print("Spray CheckList5")
+            for j in range (0, COL):
+                #print("Spray CheckList6")
+                if matrix[((counter1 - 1) * ROW + i)][j] != -1 :
+                    #print("Spray CheckList7")
+                    tempStr = tempStr + "0 0 0"
+                else :
+                    #print("Spray CheckList8")
+                    tempStr = tempStr + "2 2 2"
+                #print("Spray CheckList9")
+                tempStr = tempStr + " "
+    else :
+        #print("Spray CheckList10")
+        for i in range (0,ROW):
+            #print("Spray CheckList11")
+            for j in range (0, COL):
+                #print("Spray CheckList12")
+                if matrix[((counter1 - 1) * ROW + i)][j] != -1 :
+                    #print("Spray CheckList13")
+                    tempStr = tempStr + "0 0 0"
+                else :
+                    #print("Spray CheckList14")
+                    tempStr = tempStr + "2 2 2"
+                #print("Spray CheckList15")
+                tempStr = tempStr + " "
 
-    print("spray Function ended")
-    return retList
-
+    print("Spray Function ended")
+    #return retList
+    return tempStr
 
 host = ''
 port = 9000
@@ -97,11 +150,11 @@ server_socket.listen(1)
 # Accept a single connection and make a file-like object out of it
 #connection = server_socket.accept()[0].makefile('rb')
 connection1, address = server_socket.accept()
-t0 = time.time() + delay
+#t0 = time.time() + delay
 connection = connection1.makefile('rb')
 # connection1 address = server_socket.accept()
 print("Connected to: " + address[0] + ":" + str(address[1]))
-count = 1
+count = 0
 try:
     while True:
         
@@ -118,50 +171,62 @@ try:
         # processing on it
         image_stream.seek(0)
         
-        imageName = "t/" + str(count) + 'out.jpeg'
+        count = count + 1
+        imageName1 = "t/" + str(count) + 'out.jpeg'
+        imageName2 = str(count) + 'out.jpeg'
         
-        image = Image.open(image_stream).save(imageName)
+        image = Image.open(image_stream).save(imageName2)
         
-        pathname = "./darknet detector test cfg/READY.data cfg/READY.cfg backup/READY_25000.weights " + imageName + " -thresh 0.05"
-        
+        pathname = "./darknet detector test cfg/READY.data cfg/READY.cfg backup/READY_20000.weights " + imageName1 + " -thresh 0.05"
         call([pathname],shell = True)
+        
         try:
             with open('text1.txt') as f:
                 mylist = f.read().splitlines() #below if line should be in loop
                 print("size of mylist: " + str(len(mylist)))
                 if len(mylist) > 0 :
                     print(*mylist)
-                    for i in range(len(mylist)) :
-                        if int(mylist[i+1]) >= 0 :
+                    k = 0
+                    while k < len(mylist) :
+                        print("1 => Check for counter = " + str(count) + " and k = " + str(k))
+                        afterK = int(mylist[k + 1])
+                        print("afterK = " + str(afterK))
+                        if afterK >= 0:
                             #colorPicture(str(mylist[0]), int(mylist[1]), int(mylist[2]), int(mylist[3]), int(mylist[4]))
-                            colorPicture(counter, str(mylist[i]), int(mylist[i+1]), int(mylist[i+2]), int(mylist[i+3]), int(mylist[i+4]))
-                            i  = i + 4
+                            print("2 => Check for counter = " + str(count) + " and k = " + str(k))
+                            colorPicture(int(count), int(mylist[k]), int(mylist[k + 1]), int(mylist[k + 2]), int(mylist[k + 3]), int(mylist[k+4]))
+                            print(*matrix)
                         else :
-                            i = i + 1
-                    
-                    spraylist = spray(counter)
-                    strArray = "0 0 2 0 0 0 " + "0 0 0 2 0 0 " + "0 0 0 0 2 0 "
-                    #for m in range(0, len(spraylist)-1):
-                    
-                    #print(*spraylist)
-                    #connection1.send(str.encode(spraylist)) !!! NEED TO CONVERT TO STR
-                    connection1.send(str.encode(strArray))
-                    print("Check 7")
+                            print("2 => Check for counter = " + str(count) + " and k = " + str(k))
+                            k = k + 1
+                            colorPicture(int(count), int(-2), int(mylist[k + 1]), int(mylist[k + 2]), int(mylist[k + 3]), int(mylist[k + 4]))
+                        k = k + 5
+
+                    print("FULL MATRIX AFTER IMAGE " + str(count) + "=> ") #*matrix)
+                    spraylist = spray(int(count))
+                    #spraylist = "2 2 0 0 0 0 " + "0 0 0 0 0 0 " + "0 0 0 0 0 0 "
+                    #spraylist = "2 2 2 0 0 0 " + "0 0 0 2 2 2 " + "2 2 2 2 2 2 "
+                    print(str(spraylist))
+                    connection1.send(str.encode(spraylist)) #!!! NEED TO CONVERT TO STR
+                    #connection1.send(str.encode(strArray))
+                
+                    print("Check Last")
                     print(str(count) + '. Data was sent!\n')
                 else :
                     connection1.send(str.encode("No Object determined -> NO INPUT"))
                     print(str(count) + ". No Object determined -> NO INPUT")
         except:
             print(str(count) + '. File could not open. No such a file.')
-            connection1.send(str.encode("NO INPUT"))
-
+            connection1.send(str.encode("NO INPUT. Entered Exception"))
+    
+        pathname1 = "mv predictions.jpg predictions" + str(count) + ".jpg"
+        call([pathname1],shell = True)
+        
         print(str(count) + '. Image is verified')
         print(str(count) + '. Data could not be sent!\n')
-        picture = [[0 for i in range(COL)] for j in range(TOTALROW)]
 
         #print('Image is %dx%d' % image.size)
         #image.verify()
-        count = count + 1
 finally:
     connection.close()
     server_socket.close()
