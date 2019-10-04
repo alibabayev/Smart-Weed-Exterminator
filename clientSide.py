4790ae115501309c5a7108b20fba024062dea650
Ali Babayev <ali.babayev.ug@gmail.com>
2019-10-04 17:25:54 +0300

    Update clientSide.py

--- clientSide.py
+++ clientSide.py
@@ -3,25 +3,24 @@ import socket
 import struct
 import time
 import picamera
-import RPi.GPIO as GPIO
+import serial
 
 # Connect a client socket to my_server:8000 (change my_server to the
 # hostname of your server)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('169.254.236.148', 7000))
+client_socket.connect(('169.254.239.139', 9000))
 
-servoPIN = 17
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(servoPIN, GPIO.OUT)
 
-p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-p.start(2.5) # Initialization
+ser = serial.Serial("/dev/ttyUSB0",9600)
+ser.flushInput()
 
-# Make a file-like object out of the connection
+numOfimages = 5
+
+#Make a file-like object out of the connection
 connection = client_socket.makefile('wb')
 try:
     camera = picamera.PiCamera()
-    camera.resolution = (720, 540)
+    camera.resolution = (2880, 2160)
     # Start a preview and let the camera warm up for 2 seconds
     camera.start_preview()
     time.sleep(2)
@@ -30,13 +29,13 @@ try:
     # temporarily (we could write it directly to connection but in this
     # case we want to find out the size of each capture first to keep
     # our protocol simple)
-    start = time.time()
     stream = io.BytesIO()
     count = 0
+    start = time.time()
     startTime = time.time()
     for foo in camera.capture_continuous(stream, 'jpeg'):
-        # If we've been capturing for more than 30 seconds, quit
-        if time.time() - start > 10:
+        # If we've been capturing for more than 10 seconds, quit
+        if time.time() - start > ((5 * numOfimages) - 2):
             break
         # Write the length of the capture to the stream and flush to
         # ensure it actually gets sent
@@ -46,29 +45,28 @@ try:
         stream.seek(0)
         connection.write(stream.read())
         reply = client_socket.recv(1024)
-        print(reply.decode('utf-8'))
-        
-        #servo motor code starts
-        p.ChangeDutyCycle(7.5)  # turn towards 90 degree
-        time.sleep(1) # sleep 1 second
-        p.ChangeDutyCycle(2.5)  # turn towards 0 degree
-        time.sleep(1) # sleep 1 second
-        p.ChangeDutyCycle(12.5) # turn towards 180 degree
-        time.sleep(1) # sleep 1 second 
-        #servo motor code end
-        
+        string1 = reply.decode('utf-8')
+        #string1 = '0 0 0 0 2 2 0 0 2 2 0 0 2 2 0 0 0 0'
+        print(string1)
+        endTime1 = time.time() - startTime
+        print(endTime1)
+        time.sleep((4.5 - endTime1))
+        print('basladi')
+        #if count == 0 :
+            #time.sleep(27)
+        ser.write(string1.encode())
+        print('bitdi')
         # Reset the stream for the next capture
         stream.seek(0)
         stream.truncate()
-        
-        endTime = time.time() - startTime
-        print(endTime)
+        endTime2 = time.time() - startTime
+        print((endTime2))
+        time.sleep(5 - endTime2)
+        print((time.time() - startTime))
         count = count + 1
         startTime = time.time()
-    # Write a length of zero to the stream to signal we're done
+    #Write a length of zero to the stream to signal we're done
     connection.write(struct.pack('<L', 0))
 finally:
     connection.close()
     client_socket.close()
-    p.stop()
-    GPIO.cleanup()
